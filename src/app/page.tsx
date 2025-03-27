62df708408b454591b36de853191769a1826dc10
Mehedi <thecodermehedi@gmail.com>
2025-03-27 16:34:50 +0600

    Remove unused SVG files and update homepage layout with new design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,95 +2,17 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+    <div className="flex min-h-screen items-center justify-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="flex flex-col items-center justify-center gap-[32px]">
         <Image
-          className="dark:invert"
+          src="/next-faster.svg"
           alt="Next.js logo"
-          src="/next.svg"
-          width={180}
+          width={100}
           height={38}
           priority
         />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank">
-            <Image
-              className="dark:invert"
-              alt="Vercel logomark"
-              src="/vercel.svg"
-              height={20}
-              width={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            rel="noopener noreferrer"
-            target="_blank">
-            Read our docs
-          </a>
-        </div>
+        <h1 className="text-2xl font-bold">NextStarter</h1>
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          rel="noopener noreferrer"
-          target="_blank">
-          <Image
-            src="/file.svg"
-            alt="File icon"
-            aria-hidden
-            height={16}
-            width={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          rel="noopener noreferrer"
-          target="_blank">
-          <Image
-            src="/window.svg"
-            alt="Window icon"
-            aria-hidden
-            height={16}
-            width={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          rel="noopener noreferrer"
-          target="_blank">
-          <Image
-            src="/globe.svg"
-            alt="Globe icon"
-            aria-hidden
-            height={16}
-            width={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 };
