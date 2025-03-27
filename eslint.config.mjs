537b51390cafbcdba4354387e8083858f7f17221
Mehedi <thecodermehedi@gmail.com>
2025-03-27 15:51:46 +0600

    Enhance ESLint configuration with perfectionist plugin and enforce arrow function usage

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,43 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import perfectionist from "eslint-plugin-perfectionist";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next", "next/core-web-vitals", "next/typescript", "prettier"],
+    plugins: ["prefer-arrow-functions"],
+  }),
+  perfectionist.configs["recommended-line-length"],
+  {
+    rules: {
+      "prefer-template": "error",
+      "prefer-arrow-callback": "error",
+      "no-restricted-syntax": [
+        "error",
+        {
+          selector: "FunctionExpression",
+          message: "FunctionExpression: Function syntax is illegal. Only arrow functions are allowed",
+        },
+        {
+          selector: "FunctionDeclaration",
+          message: "FunctionDeclaration: Function syntax is illegal. Only arrow functions are allowed",
+        },
+      ],
+      "prefer-arrow-functions/prefer-arrow-functions": [
+        "error",
+        {
+          allowedNames: [],
+          allowNamedFunctions: false,
+          allowObjectProperties: false,
+          classPropertiesAllowed: false,
+          disallowPrototype: false,
+          returnStyle: "unchanged",
+          singleReturnOnly: false,
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
