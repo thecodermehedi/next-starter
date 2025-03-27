9beba440d1a79b940b74750be69ffafc9e1a2588
Mehedi <thecodermehedi@gmail.com>
2025-03-27 15:52:22 +0600

    Update Next.js configuration to include image remote patterns and commented out options for future use

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // output: "standalone",
+  images: {
+    // minimumCacheTTL: 31536000, // 1 year in seconds
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       destination: `${process.env.API_URL}/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
