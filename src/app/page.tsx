27bae0ab28e18fcb54e74a137e63c35bdf32fe1a
Mehedi <thecodermehedi@gmail.com>
2025-04-13 10:12:53 +0600

    Replace static logo with imported SVG for improved maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
+import logo from "@/app/icon0.svg";
 import Image from "next/image";
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="flex flex-col items-center justify-center gap-[32px]">
+      <main className="flex flex-col items-center justify-center">
         <Image
-          src="/next-faster.svg"
           alt="Next.js logo"
           width={100}
           height={38}
+          src={logo}
           priority
         />
         <h1 className="text-2xl font-bold">NextStarter</h1>
