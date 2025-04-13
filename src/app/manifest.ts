f0130956de810754081c4d5f7a9e247e567689e7
Mehedi <thecodermehedi@gmail.com>
2025-04-13 15:41:26 +0600

    Update manifest.ts to utilize site configuration for improved metadata consistency

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,4 +1,6 @@
 import type { MetadataRoute } from "next";
+
+import { siteConfig } from "@/config/site";
 //? To preview the output manifest.json file in the browser, you can use the following URL:
 //! http://localhost:3000/manifest.webmanifest
 const manifest = (): MetadataRoute.Manifest => {
@@ -17,12 +19,12 @@ const manifest = (): MetadataRoute.Manifest => {
         sizes: "512x512",
       },
     ],
-    description: "A Next.js Starter Template",
+    description: siteConfig.description,
+    short_name: siteConfig.shortName,
     background_color: "#ffffff",
     theme_color: "#ffffff",
     display: "standalone",
-    name: "NextStarter",
-    short_name: "NS",
+    name: siteConfig.name,
     start_url: "/",
   };
 };
